966bf7dfcb3007c544ce5203c4d375a5deb5d3d9
Joshua Barretto <joshua.s.barretto@gmail.com>
2023-06-20 10:38:42 +0100

    Added public IP detection

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ struct Args {
     initial_peers: Vec<String>,
     #[arg(short, long, default_value = "[::1]")]
     address: String,
+    #[arg(short, long)]
+    url: Option<String>,
     #[arg(short, long, default_value_t = 34093)]
     port: u16,
 }
@@ -16,10 +18,20 @@ struct Args {
 async fn main() -> Result<(), Error<http::Error>> {
     let args = Args::parse();
 
-    let host_addr = format!("http://{}:{}", args.address, args.port);
+    let host_addr = if let Some(url) = args.url {
+        url
+    } else {
+        let public_ip = public_ip_addr::get_public_ip()
+            .await
+            .expect("failed to get public IP");
+        format!("http://{}:{}", public_ip, args.port)
+    };
+    let host_url = host_addr.parse().unwrap();
+    println!("Using {} as the host URL", host_url);
+
     Node::<http::Http>::new(
         PrivateId::generate(),
-        host_addr.parse().unwrap(),
+        host_url,
         args.initial_peers,
         http::Config {
             bind_addr: format!("{}:{}", args.address, args.port).parse().unwrap(),
